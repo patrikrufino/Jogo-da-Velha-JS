61b83e297031b4a580cbfc1375fd508205cfef3b
Patrik Rufino <patrik.rufino@gmail.com>
2022-04-26 11:52:25 -0300

    Initial commit

---
+++ assets/js/scripts.js
@@ -0,0 +1,16 @@
+var player = null;
+
+function selectSquare(id) {
+    
+    var square = document.getElementById(id);
+
+    square.innerHTML = player;
+    square.style.color = '#000';
+
+}
+
+function changePlayer(value) [
+
+
+    
+]
