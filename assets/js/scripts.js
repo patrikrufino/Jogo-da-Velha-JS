8c822c1a021a29a6c79a9a56415ebc7c0bc90e39
Patrik Rufino <patrik.rufino@gmail.com>
2022-05-27 18:54:14 -0300

    Mudado -> Vencedor para Placar com contador por jogador

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,6 +1,8 @@
 var jogador, vencedor = null;
 var jogadorSelecionado = document.getElementById('jogador-selecionado');
-var vencedorSelecionado = document.getElementById('vencedor-selecionado');
+var vencedorJogadorO = document.getElementById('placar-jogador-o');
+var vencedorJogadorX = document.getElementById('placar-jogador-x');
+
 
 mudarJogador('X');
 
@@ -15,12 +17,14 @@ function escolherQuadrado(id) {
     }
 
     quadrado.innerHTML = jogador;
-    quadrado.style.color = '#000';
+    quadrado.style.color = '#727272';
 
     if (jogador === 'X') {
         jogador = 'O';
+        quadrado.style.color = '#EB8686';
     } else {
         jogador = 'X';
+        quadrado.style.color = '#AC86EB';
     }
 
     mudarJogador(jogador);
@@ -32,7 +36,7 @@ function mudarJogador(valor) {
     jogadorSelecionado.innerHTML = jogador;
 }
 
-function checaVencedor(){
+function checaVencedor() {
     var quadrado1 = document.getElementById(1);
     var quadrado2 = document.getElementById(2);
     var quadrado3 = document.getElementById(3);
@@ -88,18 +92,38 @@ function checaVencedor(){
     if (checaSequencia(quadrado3, quadrado5, quadrado7)) {
         mudaCorQuadrado(quadrado3, quadrado5, quadrado7);
         mudarVencedor(quadrado3);
+
+        return;
     }
+
+
 }
 
 function mudarVencedor(quadrado) {
     vencedor = quadrado.innerHTML;
-    vencedorSelecionado.innerHTML = vencedor;
+    // Verifica vencedor e adiciona ao placar
+    if (vencedor === 'X') {
+        vencedorJogadorX.innerHTML = parseInt(vencedorJogadorX.innerHTML) + 1;
+    } else {
+        vencedorJogadorO.innerHTML = parseInt(vencedorJogadorO.innerHTML) + 1;
+    }
 }
 
 function mudaCorQuadrado(quadrado1, quadrado2, quadrado3) {
-    quadrado1.style.background = '#0f0';
-    quadrado2.style.background = '#0f0';
-    quadrado3.style.background = '#0f0';
+    quadrado1.style.background = '#94EB86';
+    quadrado2.style.background = '#94EB86';
+    quadrado3.style.background = '#94EB86';
+
+    //Verifica o vencedor e muda a cor da letra para roxo ou verde
+    if (vencedor === 'X') {
+        quadrado1.style.color = '#727272';
+        quadrado2.style.color = '#727272';
+        quadrado3.style.color = '#727272';
+    } else {
+        quadrado1.style.color = '#727272';
+        quadrado2.style.color = '#727272';
+        quadrado3.style.color = '#727272';
+    }
 }
 
 function checaSequencia(quadrado1, quadrado2, quadrado3) {
@@ -112,10 +136,8 @@ function checaSequencia(quadrado1, quadrado2, quadrado3) {
     return eigual;
 }
 
-function reiniciar()
-{
+function reiniciar() {
     vencedor = null;
-    vencedorSelecionado.innerHTML = '';
 
     for (var i = 1; i <= 9; i++) {
         var quadrado = document.getElementById(i);
